b5bb8e4f16e57e04e14689874b17bbf055649cd3
Jake Plagge <plaggeij@mail.uc.edu>
2022-06-26 18:48:42 -0500

    Added the rest of the needed files

--- app/src/main/java/com/animal_villa/GameFramework.kt
+++ app/src/main/java/com/animal_villa/GameFramework.kt
@@ -1,15 +1,24 @@
 package com.animal_villa
 
 import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 //import com.animal_villa.ui.theme.AnimalVillaTheme
 import com.example.main_animal_villa.R
 //import java.lang.reflect.Modifier
 
-class GameFramework : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class GameFramework(textView: TextView, gameRunner: GameRunner){
+    val response : TextView = textView
+    val gameRunner = gameRunner
+
+    fun yesButton(){
+        response.text = gameRunner.getString(R.string.yes_selection)
+    }
+    fun noButton(){
+        response.text = gameRunner.getString(R.string.no_selection)
     }
 }
 
