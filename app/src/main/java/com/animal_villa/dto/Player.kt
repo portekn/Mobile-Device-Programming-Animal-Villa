7155dbeb53a534cb2a898583a833c473238532e3
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-07-03 13:33:53 -0400

    player varaibles

--- app/src/main/java/com/animal_villa/dto/Player.kt
+++ app/src/main/java/com/animal_villa/dto/Player.kt
@@ -2,11 +2,14 @@ package com.animal_villa.dto
 
 class Player (){
 
+<<<<<<< Updated upstream
     val name: String = "Neighbor"
     var status: int = 0
     var money: int = 20
     var energy: int = 30
     var location: int
+=======
+
 
     //Status - How much characters like the player. Out of 100
     val status = 75
@@ -17,3 +20,4 @@ class Player (){
 
     //currentLocation - Records where the player is located to trigger scenes. Starts at 0 (Home)
 }
+
