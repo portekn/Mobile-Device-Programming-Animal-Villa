edb4584d2387e66c2d900e099fb6bcff897070bf
Chloe Motz <finnickodair11@gmail.com>
2022-07-04 13:42:49 -0400

    Added Settings.kt and updating DAO files

--- app/src/main/java/com/animal_villa/dao/ICharacterListDAO.kt
+++ app/src/main/java/com/animal_villa/dao/ICharacterListDAO.kt
@@ -3,7 +3,10 @@ package com.animal_villa.dao
 import com.animal_villa.dto.Character
 import retrofit2.Call
 import androidx.room.Dao
+import retrofit2.http.GET
 
 
 interface ICharacterListDAO {
+    @GET()
+    fun getCharacter(): Call<ArrayList<Character>>
 }
--- app/src/main/java/com/animal_villa/dao/ISettingsDAO.kt
+++ app/src/main/java/com/animal_villa/dao/ISettingsDAO.kt
@@ -5,7 +5,17 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.animal_villa.dto.Settings
+
 
 
 interface ISettingsDAO {
+    @Query("Select * from Settings Limit 1")
+    fun getSettings():Settings
+
+    @Update
+    fun updateSettings(s:Settings)
+
+    @Insert
+    fun insertSettings(s:Settings)
 }
