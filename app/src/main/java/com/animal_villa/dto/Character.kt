2061585a0722ab62fd2e08e80ac1b7d340240484
Chloe Motz <finnickodair11@gmail.com>
2022-07-06 19:55:21 -0400

    Adding Firebase

--- app/src/main/java/com/animal_villa/dto/Character.kt
+++ app/src/main/java/com/animal_villa/dto/Character.kt
@@ -1,6 +1,6 @@
 package com.animal_villa.dto
 
-data class Character (val characterName: String, val characterFamily: String, var characterJob: String){
+data class Character (val characterName: String, val characterFamily: String, var characterJob: String,var characterId: Int){
     override fun toString(): String {
         return characterName.plus(" ").plus(characterJob)
     }
