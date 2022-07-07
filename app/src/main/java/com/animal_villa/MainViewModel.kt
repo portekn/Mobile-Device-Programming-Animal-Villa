8891fb4f5c6a11811fbf68d006e332d999836ff5
Chloe Motz <finnickodair11@gmail.com>
2022-07-06 20:59:37 -0400

    Working on MainViewModel.kt

--- app/src/main/java/com/animal_villa/MainViewModel.kt
+++ app/src/main/java/com/animal_villa/MainViewModel.kt
@@ -6,19 +6,18 @@ import com.animal_villa.dto.Character
 import com.animal_villa.dto.Player
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 
-//import kotlinx.coroutines.launch
-
 class MainViewModel: ViewModel(){
+
     private lateinit var firestore : FirebaseFirestore
 
     init{
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
+
 
     fun save(){
         //firestore.collection()
