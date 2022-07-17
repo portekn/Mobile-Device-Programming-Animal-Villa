4c5cb08f50d18553b24d6f4f273202cbeaac9f64
Kelli Porter <portekn@mail.uc.edu>
2022-07-17 19:38:33 -0400

    Code navigates through prompts

--- app/src/main/java/app/GamePlayModel.kt
+++ app/src/main/java/app/GamePlayModel.kt
@@ -1,54 +1,89 @@
 package app
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import app.AnimalVilla.R
 import app.DTO.Prompt
 import org.json.JSONObject
-import org.w3c.dom.Text
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 import java.util.concurrent.LinkedBlockingQueue
-import java.util.zip.Inflater
 import kotlin.concurrent.thread
 
 class GamePlayModel: AppCompatActivity() {
 
     //Variables
+    var PromptText:  String = ""//0 Text displayed to the player aka dialogue
+    var LeftOption:  String = ""//1 Text displayed to the player as a choice for left option
+    var RightOption: String = ""//2 Text displayed to the player as a choice for right option
+    var LeftEnergy:  Int    = 0 //3 How much energy will be used if selecting left option
+    var LeftMoney:   Int    = 0 //4 How much money will be gained or lost if selecting left option
+    var LeftStatus:  Int    = 0 //5 How many status points the player will gain or lose if selecting the left option
+    var RightEnergy: Int    = 0 //6 How much energy will be used if selecting the right option
+    var RightMoney:  Int    = 0 //7 How much money will be gained or lost if selecting the right option
+    var RightStatus: Int    = 0 //8 How many status points the player will gain or lose if selecting the right option
+    var NextLeft:    Int    = 0 //9 id of the next prompt to fetch for the player if selecting the left option
+    var NextRight:   Int    = 0 //10 id of the next prompt to fetch for the player if selecting the right option
+    var id:          Int    = 0 //11 id of the current prompt
+    var fetch:       Int    = 0 //Holds the id number of the next prompt to fetch
+    private var array:ArrayList<String> = arrayListOf() //Holds a list of array items for variables above. Will be used to add values to variables above
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Hides Action and Status bars
         supportActionBar?.hide()
         hideSystemBars()
+
+        //Lets the game run
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_play)
 
-        val queue = getAllPrompts()
-        //println( queue?.get(2).toString())
-        //DO STUFF HERE
+        //Gets the first prompt
+        organizeCurrentPrompt(fetch, array)
 
+        //Assigns variables for easy access
+        //assignVariables()
 
+        //Displays first prompt
         val textView = findViewById<TextView>(R.id.promptBox)
+        textView.setText(array[0])
+
+        //val textView = findViewById<TextView>(R.id.promptBox)
         //textView.setOnClickListener{ Toast.makeText(this@GamePlayModel,
         //   R.string.text_on_click, Toast.LENGTH_LONG).show() }
-        textView.text = queue?.get(2).toString()
+        //textView.text = queue?.get(2).toString()
 
+        //Do this when left button is pressed
+        val leftButton = findViewById<Button>(R.id.leftButton)
+        leftButton.setOnClickListener {
+            //left button does things here
+            fetch = array[1].toInt()
+            println(fetch)
+            organizeCurrentPrompt(fetch, array)
+            //assignVariables()
+            textView.setText(array[0])
+        }
+
+        //DO this when right button is pressed
+        val rightButton = findViewById<Button>(R.id.rightButton)
+        rightButton.setOnClickListener {
+            //right button does things here
+            fetch = array[2].toInt()
+            println(fetch)
+            organizeCurrentPrompt(fetch, array)
+            //assignVariables()
+            textView.setText(array[0])
+        }
     }
 
     //Hides the system bars when app is running
-    //Need to put somewhere else
     private fun hideSystemBars() {
         val windowInsetsController =
             WindowCompat.getInsetsController(window, findViewById(R.id.GamePlay))
@@ -56,10 +91,42 @@ class GamePlayModel: AppCompatActivity() {
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
-}
 
-private fun getAllPrompts(): MutableList<Prompt>? {
-    val queue = LinkedBlockingQueue<MutableList<Prompt>?>()
+    //Assigns variables for use
+    private fun assignVariables(){
+        array.set(index = 0,array[0])
+        array.set(index = 9,array[9])
+        array.set(index = 10,array[10])
+        array.set(index = 11,array[11])
+
+        array[1] = array[1]
+        array[2] = array[2]
+        array[3] = array[3]
+        array[4] = array[4]
+        array[5] = array[5]
+        array[6] = array[6]
+        array[7] = array[7]
+        array[8] = array[8]
+        /*
+        PromptText = array[0]
+        LeftOption = array[1]
+        RightOption= array[2]
+        LeftEnergy = array[3].toInt()
+        LeftMoney = array[4].toInt()
+        LeftStatus = array[5].toInt()
+        RightEnergy = array[6].toInt()
+        RightMoney = array[7].toInt()
+        RightStatus = array[8].toInt()
+        NextLeft = array[9].toInt()
+        NextRight = array[10].toInt()
+        id = array[11].toInt()
+
+         */
+    }
+}
+//Collects all prompts for use
+private fun getAllPrompts(): MutableList<Prompt> {
+    val queue = LinkedBlockingQueue<MutableList<Prompt>>()
     thread {
         try {
             StrictMode.setThreadPolicy(
@@ -78,32 +145,6 @@ private fun getAllPrompts(): MutableList<Prompt>? {
             inStream.close()
 
             val prompts = mutableListOf<Prompt>()
-
-            /*array way
-        val jRecords= JSONObject(data).get("record") as JSONArray;
-        println("RECORD");
-        println(jRecords.toString())
-        for (i in 0 until jRecords.length()){
-            val jPrompt=jRecords.get(i) as JSONObject
-            //println(jPrompt)
-            val promptText=jPrompt.get("PromptText").toString()
-            val energy=jPrompt.get("Energy").toString().toInt()
-            val money=jPrompt.get("Money").toString().toInt()
-            val status=jPrompt.get("Status").toString().toInt()
-            val left=jPrompt.get("Left").toString().toInt()
-            val right=jPrompt.get("Right").toString().toInt()
-            println("{" +
-                    "\n promptText: "+promptText +
-                    "\n energy: "+energy +
-                    "\n money: "+money +
-                    "\n status: "+status +
-                    "\n left: "+left +
-                    "\n right: "+right +
-                    "\n}")
-            prompts.add(Prompt(promptText,energy,money,status,left,right))
-        }
-        */
-
             val jRecord = JSONObject(data).get("record") as JSONObject;
             var i = 1;
             while (true) {
@@ -134,19 +175,7 @@ private fun getAllPrompts(): MutableList<Prompt>? {
 
                 //println(" $promptText $leftOption $leftEnergy $leftMoney $leftStatus $rightOption $rightEnergy $rightMoney $rightStatus $nextLeft $nextRight $id")
                 prompts.add(
-                    Prompt(
-                        promptText,
-                        leftOption,
-                        leftEnergy,
-                        leftMoney,
-                        leftStatus,
-                        rightOption,
-                        rightEnergy,
-                        rightMoney,
-                        rightStatus,
-                        nextLeft,
-                        nextRight,
-                        id
+                    Prompt(promptText, leftOption, leftEnergy, leftMoney, leftStatus, rightOption, rightEnergy, rightMoney, rightStatus, nextLeft, nextRight, id
                     )
                 )
                 i++
@@ -158,3 +187,18 @@ private fun getAllPrompts(): MutableList<Prompt>? {
     }
     return queue.take();
 }
+
+//Collects needed prompt for use
+private fun organizeCurrentPrompt(fetch:Int, array: ArrayList<String>): ArrayList<String> {
+
+    //Assign values to prompts
+    val queue = getAllPrompts()
+
+    array.add(0,queue[fetch].PromptText)
+    array.add(1,queue[fetch].NextLeft.toString())
+    array.add(2,queue[fetch].NextRight.toString())
+    array.add(3,queue[fetch].id.toString())
+
+    //Return array for use
+    return array
+    }
--- app/src/main/java/app/MainActivity.kt
+++ app/src/main/java/app/MainActivity.kt
@@ -2,15 +2,9 @@ package app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.*
-import androidx.core.view.ViewCompat.getWindowInsetsController
 import app.AnimalVilla.R
 //import com.google.firebase.auth.FirebaseUser
 //import com.google.firebase.firestore.FirebaseFirestore
@@ -18,30 +12,10 @@ import app.AnimalVilla.R
 open class MainActivity : AppCompatActivity() {
 
     //Variables
-    private lateinit var mainViewModel: MainViewModel
-    private lateinit var gameplay: GamePlayModel
-    private var textView: TextView? = null
 
     //Firebase
     //private var firebaseUser: FirebaseUser? = null
     //private latent var firestore: FirebaseFirestore
-
-    private fun startGame(view: View) {
-        //setContentView(R.layout.game_play)
-        //mainViewModel = MainViewModel(findViewById(R.id.promptBox), this)
-        //gameplay = GamePlayModel(findViewById<TextView>(R.id.promptBox),this)
-
-        //Buttons
-        //val startButton = findViewById<Button>(R.id.StartButton)
-        //val leftButton = findViewById<Button>(R.id.leftButton)
-        //val rightButton = findViewById<Button>(R.id.rightButton)
-
-        //Button Listeners
-        //startButton.setOnClickListener(StartButtonClicked())
-        //leftButton.setOnClickListener(LeftButtonClicked(gameplay))
-        ///rightButton.setOnClickListener(RightButtonClicked(gameplay))
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
@@ -58,29 +32,12 @@ open class MainActivity : AppCompatActivity() {
     }
 
     //Hides the system bars when app is running
-    //Need to put somewhere else
     private fun hideSystemBars() {
         val windowInsetsController =
             WindowCompat.getInsetsController(window, findViewById(R.id.TitleScreen))
         windowInsetsController.systemBarsBehavior =
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-        }
-    }
-
-    //Do something when left button is pressed
-    class LeftButtonClicked(gameplay: GamePlayModel) : View.OnClickListener {
-        private val mainView = gameplay
-        override fun onClick(view: View) {
-            //mainView.leftButton()
-        }
-    }
-
-    //Do something when right button is pressed
-    class RightButtonClicked(gameplay: GamePlayModel) : View.OnClickListener {
-        private val mainView = gameplay
-        override fun onClick(view: View) {
-            //mainView.rightButton()
         }
     }
 
