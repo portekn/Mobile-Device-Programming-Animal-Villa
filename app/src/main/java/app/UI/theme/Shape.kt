b317ec3d1b1d6e040854c39142340505df6b4482
Kelli Porter <portekn@mail.uc.edu>
2022-07-27 20:29:19 -0400

    Struggling

--- app/src/main/java/app/UI/theme/Shape.kt
+++ app/src/main/java/app/UI/theme/Shape.kt
@@ -3,8 +3,3 @@ package app.UI.theme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
-val Shapes = androidx.compose.material.Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
-)
